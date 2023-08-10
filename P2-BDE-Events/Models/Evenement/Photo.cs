83ba3e6dddb47a50e4812dd3723d7705b59374ea
dablusl <127994387+dablusl@users.noreply.github.com>
2023-08-10 09:05:54 -0500

    creation class Photo et son enum de Etat

---
+++ P2-BDE-Events/Models/Evenement/Photo.cs
@@ -0,0 +1,13 @@
+﻿using P2_BDE_Events.Models.Evenement.Enums;
+
+namespace P2_BDE_Events.Models.Evenement
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string PhotoPath { get; set; }
+        public int IdAuthor { get; set; }
+        public EtatPhoto Etat {get; set; }
+        public int IdAlbum { get; set; }
+    }
+}
