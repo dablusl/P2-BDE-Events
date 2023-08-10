2e7a54754eaee0eabcc4e51d7c45badaac6d3bfd
dablusl <127994387+dablusl@users.noreply.github.com>
2023-08-10 08:01:22 -0500

    init branch modelEvenement et creation class Evenement

---
+++ P2-BDE-Events/Models/Evenement/Evenement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P2_BDE_Events.Models.Evenement
+{
+    public class Evenement
+    {
+        public int Id { get; set; }
+        public string Titre { get; set; }
+        public DateTime DateEvenement { get; set; }
+        public DateTime DateLimiteInscription { get; set; }
+        public string Description { get; set; }
+        public string CoverPhotoPath { get; set; }
+        public int MaxParticipants { get; set; }
+        public int MinParticipants { get; set; }
+        public double PrixBillet { get; set; }
+        public int IdOrganisateur { get; set; }
+        public int IdAlbum { get; set; }
+
+    }
+}
