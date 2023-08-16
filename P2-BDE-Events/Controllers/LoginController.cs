e88b0fd7ba62f8865e0c6d7592cad2592b410485
Tadeus34 <thade_bossof_94@msn.com>
2023-08-16 14:36:28 +0200

    creation viewmodels view et controllers

---
+++ P2-BDE-Events/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P2_BDE_Events.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
