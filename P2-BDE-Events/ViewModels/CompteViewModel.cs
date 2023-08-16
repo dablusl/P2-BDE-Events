67ef8abcead72a3fd31f34b9d609623d494cdec3
Tadeus34 <thade_bossof_94@msn.com>
2023-08-16 14:36:28 +0200

    creation viewmodels view et controllers

---
+++ P2-BDE-Events/ViewModels/CompteViewModel.cs
@@ -0,0 +1,10 @@
+﻿using P2_BDE_Events.Models.Compte;
+
+namespace P2_BDE_Events.ViewModels
+{
+    public class CompteViewModel
+    {
+        public Compte Compte { get; set; }
+        public bool Authentifie { get; set; }
+    }
+}
