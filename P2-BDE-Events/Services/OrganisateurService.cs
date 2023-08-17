0c312459c990776f754730945f54b30b39b428de
dablusl <127994387+dablusl@users.noreply.github.com>
2023-08-17 06:51:00 -0500

    init service organisateur

--- P2-BDE-Events/Services/OrganisateurService.cs
+++ P2-BDE-Events/Services/OrganisateurService.cs
@@ -1,27 +1,53 @@
 ﻿using P2_BDE_Events.DataAccessLayer;
 using P2_BDE_Events.Models.Compte;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P2_BDE_Events.Services
 {
-    public class OrganisateurService : Dal
+    public class OrganisateurService
     {
-        public int Creer(Organisateur organisateur)
+        private readonly BDDContext _bddContext;
+
+        public OrganisateurService(BDDContext bddContext)
+        {
+            _bddContext = bddContext;
+        }
+        public int CreerOrganisateur(Organisateur organisateur)
         {
             _bddContext.Organisateurs.Add(organisateur);
             _bddContext.SaveChanges();
             return organisateur.Id;
         }
 
-        public void Modifier(int id, Organisateur modifications)
+        public void ModifierOrganisateur(int id, Organisateur modifications)
         {
             Organisateur cible = _bddContext.Organisateurs.Find(id);
             if (cible == null)
             {
                 cible = modifications;
-                _bddContext.SaveChanges(true);
+                _bddContext.SaveChanges();
             }
         }
 
+        public Organisateur  ObtenirOrganisateur(int id)
+        {
+            return _bddContext.Organisateurs.Find(id);
+        }
 
+        public List<Organisateur> ObtenirTousLesOrganisateurs()
+        {
+            return _bddContext.Organisateurs.ToList();
+        }
+
+        public void SupprimerOrganisateur(int id)
+        {
+            Organisateur cible = _bddContext.Organisateurs.Find(id);
+            if (cible == null)
+            {
+                _bddContext.Organisateurs.Remove(cible);
+                _bddContext.SaveChanges();
+            }
+        }
     }
 }
