f54b3663c90eff579a55007ff40b36997f2680e5
Tadeus34 <thade_bossof_94@msn.com>
2023-08-17 15:52:43 +0200

    suppression compte service et copie dans authentification

--- P2-BDE-Events/Services/AuthentificationService.cs
+++ P2-BDE-Events/Services/AuthentificationService.cs
@@ -37,5 +37,50 @@ namespace P2_BDE_Events.Services
             return BitConverter.ToString(new MD5CryptoServiceProvider().ComputeHash(ASCIIEncoding.Default.GetBytes(motDePasseSel)));
         }
 
+
+        //private BDDContext _bddContext;
+        //public CompteService()
+        //{
+        //    _bddContext = new BDDContext();
+        //}
+        public List<Organisateur> ObtenirTousLesOrganisateurs()
+        {
+            return _bddContext.Organisateurs.ToList();
+        }
+
+        public int AjouterOrganisateur(string email, string password)
+        {
+            string motDePasse = EncodeMD5(password);
+            Organisateur orga = new Organisateur() { Email = email, MotDePasse = motDePasse };
+            this._bddContext.Comptes.Add(orga);
+            this._bddContext.SaveChanges();
+            return orga.Id;
+        }
+
+
+
+        public static string EncodeMD5(string motDePasse)
+        {
+            string motDePasseSel = "BDEEVENTS" + motDePasse + "ASP.NET MVC";
+            return BitConverter.ToString(new MD5CryptoServiceProvider().ComputeHash(ASCIIEncoding.Default.GetBytes(motDePasseSel)));
+        }
+
+
+
+        //public Compte ObtenirCompte(int id)
+        //{
+        //    return this._bddContext.Comptes.Find(id);
+        //}
+        //public Compte ObtenirCompte(string idStr)
+        //{
+        //    int id;
+        //    if (int.TryParse(idStr, out id))
+        //    {
+        //        return this.ObtenirCompte(id);
+        //    }
+        //    return null;
+
+        //}
+
     }
 }
