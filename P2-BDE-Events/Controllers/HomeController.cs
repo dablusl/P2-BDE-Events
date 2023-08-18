652ffe44ac42e3375eaca4aa0becab2f6d6955ff
dablusl <127994387+dablusl@users.noreply.github.com>
2023-08-18 09:29:42 -0500

    init home index.cshtml index.css et layout visitor

---
+++ P2-BDE-Events/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P2_BDE_Events.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
