bfeaeaf9be68ed22ec906bc8a0b4448d3da7e356
Votre Nom Complet <votreemail@exemple.fr>
2023-08-23 12:21:29 +0200

    bugFormulaire

--- P2-BDE-Events/ViewModels/EvenementViewModel.cs
+++ P2-BDE-Events/ViewModels/EvenementViewModel.cs
@@ -1,4 +1,5 @@
-﻿using P2_BDE_Events.Models.Evenement;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using P2_BDE_Events.Models.Evenement;
 using P2_BDE_Events.Models.Evenement.Enums;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,8 @@ namespace P2_BDE_Events.ViewModels
     public class EvenementViewModel
     {
         public Evenement Evenement { get; set; }
+
+        public TypeEvenement SelectedType { get; set; }
 
         public List<TypeEvenement> TypeEvenements { get; set;}
     }
