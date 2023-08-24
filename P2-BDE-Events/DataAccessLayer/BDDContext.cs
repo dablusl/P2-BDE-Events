2a9a765e989bf34889969d862d75bb6b4dab651f
Exaron <135147510+Exaron@users.noreply.github.com>
2023-08-24 16:58:50 +0200

    Bd error key FaturePresta ?

--- P2-BDE-Events/DataAccessLayer/BDDContext.cs
+++ P2-BDE-Events/DataAccessLayer/BDDContext.cs
@@ -15,7 +15,7 @@ namespace P2_BDE_Events.DataAccessLayer
         public DbSet<Participant> Participants { get; set; }
         public DbSet<Prestataire> Prestataires { get; set; }
         public DbSet<Prestation> Prestations { get; set; }
-        public DbSet<FacturePrestation> FacturePrestations { get; set; }
+        public DbSet<FacturePrestation> FacturePrestation { get; set; }
         public DbSet<Administrateur> Administrateurs { get; set; }
         public DbSet<Evenement> Evenements { get; set; }
         public DbSet<Litige> Litiges { get; set; }
@@ -223,80 +223,50 @@ namespace P2_BDE_Events.DataAccessLayer
                     IdAuthor = 5,
                 });
 
-            this.Prestations.AddRange(
-          new Prestation
-          {
-              Id = 1,
-              Titre = "Ma Prestation",
-              Type = Models.Prestations.Enums.TypeDePrestation.SALLE,
-              CapaciteMax = 10,
-              Tarif = 100,
-              Calendrier = "Du 1er au 5 août",
-              Livraison = true,
-              Description = "Une description de la prestation",
-              Etat = EtatDePrestation.EnAttenteDeValidation
-          });
-            new Prestation
-            {
-                Id = 2,
-                Titre = "Votre Dj",
-                Type = Models.Prestations.Enums.TypeDePrestation.DJ,
-                CapaciteMax = 50,
-                Tarif = 200,
-                Calendrier = "Tout août",
-                Livraison = true,
-                Description = "Dj pour vos soirée",
-                Etat = EtatDePrestation.EnCours
-            });
-            new Prestation
-            {
-                Id = 3,
-                Titre = "Ma Prestation",
-                Type = Models.Prestations.Enums.TypeDePrestation.TRAITEUR,
-                CapaciteMax = 300,
-                Tarif = 1000,
-                Calendrier = "3eme weekend du mois",
-                Livraison = true,
-                Description = "Traiteur spécialisé en calamar",
-                Etat = EtatDePrestation.Annulee
-            });
-            new FacturePrestation
-            {
-                NumeroFacture = 20230101,
-                Date = 2023 / 01 / 01,
-                MontantHT = 1000,
-                Prestation = 3
-            });
-            new FacturePrestation
-            {
-                NumeroFacture = 20230201,
-                Date = 01 / 02 / 2023,
-                MontantHT = 2000,
-                Prestation = 2
-            });
-            new FacturePrestation
-            {
-                NumeroFacture = 20230506,
-                Date = 06 / 05 / 2023,
-                MontantHT = 500,
-                Prestation = 2
-            });
-            new FacturePrestation
-            {
-                NumeroFacture = 20231011,
-                Date = 11 / 10 / 2023,
-                MontantHT = 3000,
-                Prestation = 3
-            });
-            new FacturePrestation
-            {
-                NumeroFacture = 20230706,
-                Date = 2023 / 07 / 06,
-                MontantHT = 5000,
-                Prestation = 1
-            });
+                 this.FacturePrestation.AddRange(
+                 new FacturePrestation
+                 {
+                     IdFacture = 1,
+                     NumeroFacture = 20230101,
+                     Date = new DateTime(2023, 01, 01),
+                     MontantHT = 1000,
+                     IdPrestation = 3 
+                 },
+                 new FacturePrestation
+                 {
+                     IdFacture = 2,
+                     NumeroFacture = 20230201,
+                     Date = new DateTime(2023, 02, 01),
+                     MontantHT = 2000,
+                     IdPrestation = 2
+                 },
+                 new FacturePrestation
+                 {
+                     IdFacture = 3,
+                     NumeroFacture = 20230506,
+                     Date = new DateTime(2023, 05, 06),
+                     MontantHT = 500,
+                     IdPrestation = 2
+                 },
+                 new FacturePrestation
+                 {
+                     IdFacture = 4,
+                     NumeroFacture = 20231011,
+                     Date = new DateTime(2023, 10, 11),
+                     MontantHT = 3000,
+                     IdPrestation = 3
+                 },
+                 new FacturePrestation
+                 {
+                     IdFacture = 5,
+                     NumeroFacture = 20230706,
+                     Date = new DateTime(2023, 07, 06),
+                     MontantHT = 5000,
+                     IdPrestation = 1
+                 });
 
-            this.SaveChanges();
-        }
-    }
+                 this.SaveChanges();
+
+                }
+            }
 }
--- P2-BDE-Events/Models/Prestations/FacturePrestation.cs
+++ P2-BDE-Events/Models/Prestations/FacturePrestation.cs
@@ -4,9 +4,11 @@ namespace P2_BDE_Events.Models.Prestations
 {
     public class FacturePrestation
     {
+        public int IdFacture { get; set; }
         public int NumeroFacture { get; set; }
         public DateTime Date { get; set; }
         public decimal MontantHT { get; set; }
-        public virtual Prestation Prestation{ get; set; }
+        public int IdPrestation { get; set; }
+        public Prestation Prestation { get; set; }
     }
 }
