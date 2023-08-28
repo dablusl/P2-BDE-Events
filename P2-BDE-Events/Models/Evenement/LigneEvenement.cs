d4cec8c664ac02ded719cc264927df49dc4dc6ef
Gonzalo B <127994387+dablusl@users.noreply.github.com>
2023-08-28 04:03:34 -0500

    Update LigneEvenement.cs
    ajouter Key

--- P2-BDE-Events/Models/Evenement/LigneEvenement.cs
+++ P2-BDE-Events/Models/Evenement/LigneEvenement.cs
@@ -7,8 +7,10 @@ namespace P2_BDE_Events.Models.Evenement
     public class LigneEvenement
     {
         public virtual Prestation Prestation { get; set; }
+        [Key]
         [Required]
         public virtual Evenement Evenement { get; set; }
+        [Key]
         [Required]
         public TypeDePrestation Type { get; set; }
 
