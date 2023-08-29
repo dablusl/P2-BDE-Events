a84370ef391f906b0010b8bf0999bc4802eae736
dablusl <127994387+dablusl@users.noreply.github.com>
2023-08-29 05:27:07 -0500

    init controlleur appel doffre

--- P2-BDE-Events/Controllers/PrestataireController/CreerUnePrestationController.cs
+++ P2-BDE-Events/Controllers/PrestataireController/CreerUnePrestationController.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using P2_BDE_Events.Models.Comptes;
+using P2_BDE_Events.Models.Prestations;
+using P2_BDE_Events.Models.Prestations.Enums;
+using P2_BDE_Events.Services.Comptes;
+using P2_BDE_Events.Services.Evenements;
+using System.Collections.Generic;
 
 namespace P2_BDE_Events.Controllers.PrestataireController
 {
@@ -6,7 +14,18 @@ namespace P2_BDE_Events.Controllers.PrestataireController
     {
         public IActionResult Index()
         {
+            Prestataire prestataire = new PrestataireService().GetPrestataireParCompte(GetIdCompte());
+            //List<TypeDePrestation> typesDePrestationDuPrestataire = prestataire.Prestations.Map
+
+            List<Prestation> appelDOffre = new LigneEvenementService().AppelsDoffre(prestataire.Prestations);
+
             return View();
         }
+
+        public int GetIdCompte()
+        {
+            return int.Parse(HttpContext.Session.GetString("iDCompte"));
+        }
+
     }
 }
