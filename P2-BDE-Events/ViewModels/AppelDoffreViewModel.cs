74f35abbba3706ebf9d10142eb0acc65eb92220e
dablusl <127994387+dablusl@users.noreply.github.com>
2023-08-29 07:59:20 -0500

    init view appel doffre et mise a jour class view model simplifie

---
+++ P2-BDE-Events/ViewModels/AppelDoffreViewModel.cs
@@ -0,0 +1,14 @@
+﻿using P2_BDE_Events.Models.Evenement;
+using P2_BDE_Events.Models.Prestations.Enums;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace P2_BDE_Events.ViewModels
+{
+    public class AppelDoffreViewModel
+    {
+        public List<TypeDePrestation> Types { get; set; }
+        public List<Evenement> EvenementsEnAppelDoffre { get; set; }
+    }
+}
