e85465cc66401f04a95d2e5b49ebb770e54146a0
dablusl <127994387+dablusl@users.noreply.github.com>
2023-08-30 03:34:33 -0500

    resolution conflit 2/8

--- P2-BDE-Events/Controllers/PrestataireController/AppelsDoffreController.cs
+++ P2-BDE-Events/Controllers/PrestataireController/AppelsDoffreController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using P2_BDE_Events.Models.Comptes;
+using P2_BDE_Events.Models.Prestations;
+using P2_BDE_Events.Services.Comptes;
+using P2_BDE_Events.Services.Evenements;
+using System.Collections.Generic;
 
 namespace P2_BDE_Events.Controllers.PrestataireController
 {
@@ -7,7 +13,17 @@ namespace P2_BDE_Events.Controllers.PrestataireController
         //afficher toutes les appels d'offre possibles par prestations du prestataire
         public IActionResult Index()
         {
+            Prestataire prestataire = new PrestataireService().GetPrestataireParCompte(GetIdCompte());
+            //List<TypeDePrestation> typesDePrestationDuPrestataire = prestataire.Prestations.Map
+
+            List<Prestation> appelDOffre = new LigneEvenementService().AppelsDoffre(prestataire.Prestations);
+
             return View();
+        }
+
+        public int GetIdCompte()
+        {
+            return int.Parse(HttpContext.Session.GetString("iDCompte"));
         }
     }
 }
