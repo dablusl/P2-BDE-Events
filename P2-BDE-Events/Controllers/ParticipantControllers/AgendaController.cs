04df5996bed626f57f70157a2086ce36ceedfd94
Tadeus34 <thade_bossof_94@msn.com>
2023-08-30 12:12:36 +0200

    affiche tous les événements dans l'agenda evenement + fonction recherche et fait le lien avec reserver événements. RAF : tri des événements dans agenda selon BDE ou Univ + afficher le nombre de participant inscrit + afficher plus d'info sur la page reserverevenement

--- P2-BDE-Events/Controllers/ParticipantControllers/AgendaController.cs
+++ P2-BDE-Events/Controllers/ParticipantControllers/AgendaController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using P2_BDE_Events.Models.Evenement;
+using P2_BDE_Events.Services.Evenements;
+using P2_BDE_Events.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P2_BDE_Events.Controllers.ParticipantControllers
 {
     public class AgendaController : Controller
     {
-        public IActionResult AgendaEvenement()
+        private EvenementService EvenementService;
+
+        public AgendaController()
         {
-            return View("Views/Participant/AgendaEvenement.cshtml");
+            EvenementService = new EvenementService();
+        }
+        //public IActionResult AgendaEvenement()
+        //{
+        //    return View("Views/Participant/AgendaEvenement.cshtml");
+        //}
+
+        [HttpGet]
+        public IActionResult AgendaEvenement(int? eventId)
+        {
+            List<Evenement> evenements = EvenementService.ObtenirTousLesEvenements(); // Obtenez la liste complète des événements
+
+            if (eventId.HasValue)
+            {
+                evenements = evenements.Where(e => e.Id == eventId.Value).ToList(); // Filtrer les événements par identifiant
+            }
+
+            // Créez le modèle pour la vue
+            var viewModel = new AgendaEvenementViewModel
+            {
+                Evenements = evenements
+            };
+
+            return View("Views/Participant/AgendaEvenement.cshtml", viewModel);
         }
     }
 }
---
+++ P2-BDE-Events/ViewModels/AgendaEvenementViewModel.cs
@@ -0,0 +1,14 @@
+﻿using P2_BDE_Events.Models.Comptes;
+using P2_BDE_Events.Models.Evenement;
+using System.Collections.Generic;
+
+namespace P2_BDE_Events.ViewModels
+{
+    public class AgendaEvenementViewModel
+    {
+        public List<Evenement> Evenements { get; set; }
+        public List<Participant> Participants { get; set; }
+
+
+    }
+}
