06847d428ab5208e85f9755974006e62c533f9eb
Exaron <135147510+Exaron@users.noreply.github.com>
2023-09-01 22:44:27 +0200

    ajout des required dans model Prestation à jour

--- P2-BDE-Events/Models/Prestations/Prestation.cs
+++ P2-BDE-Events/Models/Prestations/Prestation.cs
@@ -8,16 +8,29 @@ namespace P2_BDE_Events.Models.Prestations
 {
     public class Prestation
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Titre { get; set; }
+        
+        [Required]
         public int CapaciteMax { get; set; }
+        [Required]
         public int Tarif { get; set; }
         public DateTime Calendrier { get; set; }
+        [Required]
         public bool Livraison { get; set; }
-        public string Description { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; } = null!;
+
+
         public EtatDePrestation Etat { get; set; }
+        [Required]
         public TypeDePrestation Type { get; set; }
        // public TypeDeLocation Location { get; set; }
+        
         public int PrestataireId { get; set; }
         public virtual Prestataire Prestataire { get; set; }
     }
