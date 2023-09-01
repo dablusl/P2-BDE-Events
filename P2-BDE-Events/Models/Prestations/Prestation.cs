8f0b5458df830866fa5e312a6285e3ed948f4f1f
Exaron <135147510+Exaron@users.noreply.github.com>
2023-09-01 23:21:01 +0200

    Ajout d'une condition dans créer Prestation + Suppression des required dans model Prestation à jour

--- P2-BDE-Events/Models/Prestations/Prestation.cs
+++ P2-BDE-Events/Models/Prestations/Prestation.cs
@@ -7,27 +7,15 @@ using System.ComponentModel.DataAnnotations;
 namespace P2_BDE_Events.Models.Prestations
 {
     public class Prestation
-    {
-        [Required]
-        public int Id { get; set; }
-        [Required]
-        public string Titre { get; set; }
-        
-        [Required]
-        public int CapaciteMax { get; set; }
-        [Required]
+    { 
+        public int Id { get; set; }  
+        public string Titre { get; set; }  
+        public int CapaciteMax { get; set; }      
         public int Tarif { get; set; }
         public DateTime Calendrier { get; set; }
-        [Required]
         public bool Livraison { get; set; }
-
-        [Required]
-        [StringLength(100)]
         public string Description { get; set; } = null!;
-
-
         public EtatDePrestation Etat { get; set; }
-        [Required]
         public TypeDePrestation Type { get; set; }
        // public TypeDeLocation Location { get; set; }
         
